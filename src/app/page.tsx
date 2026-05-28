a233f1e0c8f1cf071867e49666883d265a8019d9
Adrian Huaiquilao <yastamiro@MacBook-Pro-de-Adrian.local>
2026-05-27 21:49:09 -0400

    feat: up

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-950 text-white">
+      <h1 className="text-4xl font-bold">Hola Mundo</h1>
+    </main>
+  );
+}
