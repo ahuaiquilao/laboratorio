3487e0788ec280d048d78927fe2445a82f96f204
ahuaiquilao <ahuaiquilao@gmail.com>
2026-06-26 19:03:48 -0400

    feat: limpisr

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-950 text-white">
-      <h1 className="text-4xl font-bold">Hola Mundo</h1>
-    </main>
-  );
-}
